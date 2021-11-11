c311da8c803d9e10f1d620e67f9e356a92a5b7a1
takeshi <panda531531@gmail.com>
2021-11-11 22:57:14 +0900

    add DragGestureRecognizer sample

---
+++ DragGestureRecognizerSample/DragGestureRecognizerSample/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace DragGestureRecognizerSample
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public DelegateCommand DragCommand { get; private set; }
+        private bool CanDrag()
+        {
+            return true;
+        }
+
+        public MainPageViewModel()
+        {
+            DragCommand = new DelegateCommand(Drag, CanDrag);
+        }
+
+        private void Drag()
+        {
+            Console.WriteLine("Dragging");
+        }
+
+    }
+}
